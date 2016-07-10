f4f23a660c2bb7414df86a7695abe4630f0413b0
Emma Hoppmann <emma@hoppmann.me>
2016-07-10 15:03:59 -0400

    minor tweaks

--- flask-webapp/app.py
+++ flask-webapp/app.py
@@ -293,4 +293,4 @@ def close_connection(exception):
         db.close()
 
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1',port=8000)
+    app.run(debug=False,host='127.0.0.1',port=8000)
